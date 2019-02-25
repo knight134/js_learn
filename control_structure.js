9d0f799ad04d4ccf8c6d52f8043768f7afb755f0
knight134 <knight134@126.com>
2019-02-25 20:49:13 +0800

    更新函数代码

--- control_structure.js
+++ control_structure.js
@@ -5,3 +5,13 @@ while(funds>1 && funds<100)
     funds+=3;
     console.log("now funds is:"+funds)
 }
+
+function rand(m,n)
+{
+    return m+Math.floor((n-m+1)*Math.random());
+}
+function randFace()
+{
+    return ["crown","anchor","heart","spade","club","diamand",][rand[0,5]];
+}
+
